4b0a7012b017acaddb3eeb6461909b6710ffe05d
phananhthoai <kenkukhanh@gmail.com>
2024-11-06 07:48:54 +0000

    edit

--- backup.sh
+++ backup.sh
@@ -9,6 +9,12 @@ if [ $(id -un) != root ]; then
 fi
 
 
+compressed () {
+  cd ${path}
+  sleep 7
+  sudo tar -zcvf "/mnt/backups/${namespace}/${pv}.tgz" .
+}
+
 pv=${1?'PV Required !'}
 namespace=${2?'NameSpace Required !'}
 
@@ -16,12 +22,11 @@ path=$(mount | grep ${1} | sed -E 's/\/dev\/rbd[0-9]+\s+on (.*mount).+/\1/g')
 
 if [ -f /mnt/backups/${namespace}/${pv}.tgz ]; then
   echo "File exist !!!"
-  continue
+  sudo rm -rf /mnt/backups/${namespace}/${pv}.tgz
+  compressed
 else
   sudo mkdir -p /mnt/backups/${namespace}
-  cd ${path}
-  sleep 7
-  sudo tar -zcvf "/mnt/backups/${namespace}/${pv}.tgz" .
+  compressed
 fi
 
 echo OK
--- volumes.sh
+++ volumes.sh
@@ -1,33 +1,38 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 namespace=${1?'NameSpace Required !'}
 
 scale_pod() {
 	kubectl -n ${namespace} scale ${1} ${2}  --replicas 0	
 }
-  
+up_pod() {
+  kubectl -n ${namespace} scale ${1} ${2}  --replicas 1
+}
 
-PV=$(kubectl  get pvc -n ${namespace} -o json | jq -r '.items[].spec.volumeName')
-
-for item in $(kubectl  get volumeattachments.storage.k8s.io | awk '{print $3}' | sed '1d'); do 
-  if [ ${item} == ${PV} ]; then
+for item in $(kubectl  get volumeattachments.storage.k8s.io | awk '{print $3}' | sed '1d'); do
+  if [ $(kubectl get pv ${item} -o json | jq -r '.spec.claimRef.namespace') == "${namespace}" ]; then
     csi=$(kubectl get volumeattachments.storage.k8s.io -o json | jq -r ".items[] | select(.spec.source.persistentVolumeName == \"${item}\")" | jq -r '.metadata.name')
     node=$(kubectl  get volumeattachments.storage.k8s.io ${csi} -o json | jq -r '.spec.nodeName')
-    pvc=$(kubectl get pv ${PV} -o json | jq -r '.spec.claimRef.name')
+    pvc=$(kubectl get pv ${item} -o json | jq -r '.spec.claimRef.name')
     pod=$(kubectl get -n ${namespace} pods -o json | jq -r ".items | map(select(.spec.volumes | map(select(.persistentVolumeClaim.claimName == \"${pvc}\")) | select(length > 0)) | .metadata.name) | join(\",\")")   
     if ! [ -z "${pod}" ]; then
       printf "Need scale before backup: "
       read answer
       if [ ${answer} == "y" ] || [ ${answer} == "Y" ]; then
-      	kind=$(kubectl get pods -n ${namespace} ${pod} -o json | jq -r '.metadata.ownerReferences[].kind')
-      	name=$(kubectl get pods -n ${namespace} ${pod} -o json | jq -r '.metadata.ownerReferences[].name')
-      	scale_pod "${kind}" "${name}"      	
-    		ssh elofun@${node} ./backup.sh "${PV}" "${namespace}"
-    	else
-    	  exit 1
-    	fi
-		fi
+        kind=$(kubectl get pods -n ${namespace} ${pod} -o json | jq -r '.metadata.ownerReferences[].kind')
+        name=$(kubectl get pods -n ${namespace} ${pod} -o json | jq -r '.metadata.ownerReferences[].name')
+        scale_pod "${kind}" "${name}"      	
+        ssh elofun@${node} ./backup.sh "${item}" "${namespace}"
+      else
+        exit 1
+      fi
+    fi
+    kind=$(kubectl get pods -n ${namespace} ${pod} -o json | jq -r '.metadata.ownerReferences[].kind')
+    name=$(kubectl get pods -n ${namespace} ${pod} -o json | jq -r '.metadata.ownerReferences[].name')
+    up_pod "${kind}" "${name}"
+  else
+    echo "This NameSpace has no backup plan yet !!!"
   fi
 done
